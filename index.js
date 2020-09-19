6fc7d8073afad993a4240895dadb19d85cdcba2b
Chantal Snazel <clsnazel@gmail.com>
2020-09-19 01:07:23 +0000

    Created and completed activity with recursion

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
-}
-
+  let sum = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
+};
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
 module.exports = sumItems;
